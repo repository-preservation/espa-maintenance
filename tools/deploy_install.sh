fe8d8c58ab56f6cdf8bd83eac21e3515503a3c90
David Hill <dhill@usgs.gov>
2014-05-08 21:20:40 +0000

    More updates to deploy_install
    git-svn-id: http://espa.googlecode.com/svn/trunk@736 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- tools/deploy_install.sh
+++ tools/deploy_install.sh
@@ -26,6 +26,8 @@
 #							with new app deployment with uwsgi
 #							Adding new function for stdout writing when VERBOSE is enabled
 #							Removing --remotehost flag
+#  006		05-08-2014	Adam Dosch		Adding more deployment logic for uwsgi
+#							Adding CHECKOUT_TYPE to do checkout vs export in SVN for MODE
 #
 #############################################################################################################################
 
@@ -41,11 +43,13 @@ STAMP=$( date +'%m%d%y-%H%M%S' )
 
 SVN_WORKING_DIR=${HOME}/tmp
 
-SETTINGS_FILES="/web/espa_web/espa-uwsgi.ini"
+APP_FILE="/web/espa_web/espa-uwsgi.ini"
 
 SVN_HOST="http://espa.googlecode.com"
 
 SVN_BASE="/svn"
+
+CHECKOUT_TYPE="co"
 
 declare RELEASE
 
@@ -55,11 +59,7 @@ declare TIERS="app maintenance processing"
 
 declare MODE
 
-declare MODES="test devel prod"
-
-declare DB_CRED_FILE=${HOME}/.dbnfo
-
-declare DB_CRED_SCRIPT=${HOME}/cred.regex
+declare MODES="qa devel prod"
 
 declare VERBOSE=1
 
@@ -114,21 +114,6 @@ function tier_validation
    done
 }
 
-function gen_cred_regex_script
-{
-   # Generating hack-ass script to do remote regex --- better than escaping all that shit for remote SSH command
-   cat > $DB_CRED_SCRIPT <<END
-#!/bin/bash
-. $DB_CRED_FILE
-
-sed -i.backup.h -r -e "s~^(.*'HOST': ).*(\#.*)~\1'\$h',\2~g" ${SVN_WORKING_DIR}/${SETTINGS_FILES}
-[[ -f \$0 ]] && rm -rf \$0
-END
-
-chmod +x $DB_CRED_SCRIPT
-
-}
-
 function write_stdout
 {
    # $1 -> mode
@@ -136,6 +121,22 @@ function write_stdout
    TIMESTAMP=$(  date +'%b %d %H:%M:%S' )
 
    echo "${TIMESTAMP} deployment $1: $2"   
+}
+
+function set_checkout
+{
+   # $1 -> mode
+   case $1 in
+     "prod")
+        CHECKOUT_TYPE="export"
+        ;;
+     "devel")
+        CHECKOUT_TYPE="co"
+        ;;
+     *)
+        CHECKOUT_TYPE="co"
+        ;;
+   esac
 }
 
 function deploy_tier
@@ -170,67 +171,41 @@ function deploy_tier
       lookup="${mode}-${tier}"
       for server in ${tierhosts[${lookup}]}
       do
-         continue
-####         [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Starting deployment for: $server"
-####   
-####         # Is host up? (Crude input santization might be needed? This should catch malformed FQDN or invalid hosts)
-####         [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Pinging $server ... "
-####   
-####         ${PINGBIN} -q -c2 ${server} &> /dev/null
-####   
-####         if [ $? -eq 0 ]; then
-####            [[ $VERBOSE -eq 0 ]] && echo "alive, continuing."
-####   
-####            # If we've chosen to remove releases, let's do that first, since we auto-backup prior to release below in the deployment
-####            [[ $DELETE_PRIOR_RELEASES -eq 0 ]] && echo "Removing all prior code deployments matching: ${SVN_WORKING_DIR}.deploy-*" && ${SSHBIN} -t ${server} "rm -rf ${SVN_WORKING_DIR}.deploy-*" &> /dev/null
-####   
-####            # Do code deployment on server with SSH
-####            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Deploying ESPA release $RELEASE to $SVN_WORKING_DIR on $server"
-####   
-####            ${SSHBIN} -t ${server} "mv $SVN_WORKING_DIR ${SVN_WORKING_DIR}.deploy-${STAMP}; mkdir -p $SVN_WORKING_DIR; cd $SVN_WORKING_DIR; svn co ${SVN_HOST}${SVN_BASE}/tags/${RELEASE} .; find $SVN_WORKING_DIR -type f -name \"*.pyc\" -exec rm -rf '{}' \;" &> /dev/null
-####   
-####            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Are we on a ESPA web-front-end server... "
-####   
-####            # If on the web front-end hosts, set up the DB credentials
-####            if [ "${server}" == "l8srlscp03" -o "${server}" == "l8srlscp12" ]; then
-####               [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "yes, continuing"
-####   
-####               [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Checking to see if $DB_CRED_FILE exists on $server... "
-####   
-####               # If db cred file exists on remote web host
-####               response=$( ${SSHBIN} ${server} "[[ -f $DB_CRED_FILE ]] && echo yes || echo no" )
-####   
-####               if [ "$response" == "yes" ]; then
-####                  [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "yes, continuing"
-####   
-####                  # Create db cred script
-####                  gen_cred_regex_script
-####   
-####                  [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Generating credential regex script ($DB_CRED_SCRIPT), pushing it out to $server and running that sucka..."
-####   
-####                  ${SCPBIN} -q $DB_CRED_SCRIPT ${server}:${HOME} &> /dev/null
-####                  ${SSHBIN} ${server} "$DB_CRED_SCRIPT" &> /dev/null
-####   
-####                  # Clean up locally
-####                  if [[ -f $DB_CRED_SCRIPT ]]; then
-####                     \rm -rf $DB_CRED_SCRIPT
-####                  fi
-####               else
-####                  # Couldn't find db creds, print out a warning to do it manually
-####                  if [ $VERBOSE -eq 0 ]; then
-####                     [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "failed, db credentials file does not exist.  Do DB credentials manually."
-####                  else
-####                     echo "Warning: db credentials file does not exist on $server --- database credentials were NOT input into settings.py.  Do it manually."
-####                  fi
-####               fi
-####            fi
-####   
-####            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Deployment complete for $server"
-####         else
-####            # Ping failed on host, bail out!
-####            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "failed, exiting."
-####            exit 1
-####         fi
+         echo $server $tier
+
+         [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Starting deployment for: $server"
+   
+         # Is host up? (Crude input santization might be needed? This should catch malformed FQDN or invalid hosts)
+         [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Pinging $server ... "
+   
+         ${PINGBIN} -q -c2 ${server} &> /dev/null
+
+         if [ $? -eq 0 ]; then
+            [[ $VERBOSE -eq 0 ]] && echo "alive, continuing."
+   
+            # If we've chosen to remove releases, let's do that first, since we auto-backup prior to release below in the deployment
+            [[ $DELETE_PRIOR_RELEASES -eq 0 ]] && echo "Removing all prior code deployments matching: ${SVN_WORKING_DIR}.deploy-*" && ${SSHBIN} -t ${server} "rm -rf ${SVN_WORKING_DIR}.deploy-*" &> /dev/null
+   
+            # Do code deployment on server with SSH
+            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Deploying ESPA release $RELEASE to $SVN_WORKING_DIR on $server"
+ 
+            if [ "$tier" == "app" -o "$tier" == "maintenance" ]; then
+               ${SSHBIN} -t ${server} "mv $SVN_WORKING_DIR ${SVN_WORKING_DIR}.deploy-${STAMP}; mkdir -p $SVN_WORKING_DIR; cd $SVN_WORKING_DIR; svn ${CHECKOUT_TYPE} ${SVN_HOST}${SVN_BASE}/tags/${RELEASE} .; find $SVN_WORKING_DIR -type f -name \"*.pyc\" -exec rm -rf '{}' \;" &> /dev/null
+            elif [ "$tier" == "maintenance" ]; then
+               ${SSHBIN} -t ${server} "mv $SVN_WORKING_DIR ${SVN_WORKING_DIR}.deploy-${STAMP}; mkdir -p $SVN_WORKING_DIR; cd $SVN_WORKING_DIR; svn ${CHECKOUT_TYPE} ${SVN_HOST}${SVN_BASE}/tags/${RELEASE}/tools .; find $SVN_WORKING_DIR -type f -name \"*.pyc\" -exec rm -rf '{}' \;" &> /dev/null
+            fi
+
+            # If app tier, update soft-link for uwsgi and re-touch file to reload
+            if [ "$tier" == "app" ]; then
+               ${SSHBIN} -t ${server} "ln -f -s ~/${SVN_WORKING_DIR}/${APP_FILE} /opt/cots/uwsgi/apps/; touch ~/${SVN_WORKING_DIR}/${APP_FILE}"
+            fi
+   
+            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Deployment complete for $server"
+         else
+            # Ping failed on host, bail out!
+            [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "failed, exiting."
+            exit 1
+         fi
       done
    done
 
@@ -298,6 +273,9 @@ if [ $# -ge 2 -a $# -le 4 ]; then
    else
       [[ $VERBOSE -eq 0 ]] && write_stdout "$MODE" "Passed mandatory parameter check.  We have everything to continue deployment."
    fi
+
+   # Set checkout type
+   set_checkout "$MODE"
 
    # Deploy tier
    deploy_tier "$TIER" "$MODE" 
