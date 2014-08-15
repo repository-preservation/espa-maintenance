eb5fcbd873971785bccc9f63f386c0f2b4ab01d8
Ronald Dilley <rdilley@usgs.gov>
2014-08-15 14:59:34 +0000

    Updated to inherit from a dict instead - much better
    git-svn-id: http://espa.googlecode.com/svn/trunk@1031 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/test-orders/parameters.py
+++ processing/test-orders/parameters.py
@@ -1,118 +1,167 @@
 
 import os
 import sys
+import re
 import logging
 import json
 
-class OrderParameter(object):
 
-    _name = None
+class ParameterViolation(Exception):
+    pass
 
-    def __init__(self, name):
-        self._name = name
-
-    def name(self):
-        return self._name
-
-    def value(self):
-        raise NotImplementedError("Must implement value()")
+class OptionViolation(Exception):
+    pass
 
 
-class OrderIdParameter(OrderParameter):
+class OrderParameters(dict):
 
-    _orderid = None
+    valid_parameters = ['orderid', 'scene', 'xmlrpc', 'options']
+    valid_options = None
 
-    def __init__(self, value):
-        super(OrderIdParameter, self).__init__('orderid')
-        self._orderid = value
+    def __init__(self, *args, **kwarg):
+        super(OrderParameters, self).__init__(*args, **kwarg)
 
-    def value(self):
-        return str(self._orderid)
+        parameters = self.keys()
 
+        for parameter in parameters:
+            self._is_valid_parameter(parameter)
+            self._is_missing_parameter(parameter)
 
-class SceneParameter(OrderParameter):
+        options = self['options'].keys()
 
-    _scene = None
+        for option in options:
+            self._is_valid_option(option)
+            self._is_missing_option(option)
 
-    def __init__(self, value):
-        super(SceneParameter, self).__init__('scene')
-        self._scene = value
+    def _is_valid_parameter(self, parameter):
 
-    def value(self):
-        return str(self._scene)
+        if parameter not in self.valid_parameters:
+            message = "[%s] is not a valid parameter" % parameter
+            raise ParameterViolation(message)
 
+    def _is_valid_option(self, option):
 
-class XmlrcpParameter(OrderParameter):
+        if option not in self.valid_options.keys():
+            message = "[%s] is not a valid option" % option
+            raise OptionViolation(message)
 
-    _xmlrpc = None
+    def _is_missing_parameter(self, parameter):
 
-    def __init__(self, value):
-        super(XmlrcpParameter, self).__init__('xmlrpc')
-        self._xmlrpc = value
+        if parameter not in self.valid_parameters:
+            message = "[%s] is missing from order parameters" % parameter
+            raise ParameterViolation(message)
 
-    def value(self):
-        return str(self._xmlrpc)
+    def _is_missing_option(self, option):
 
-
-class OptionsParameter(OrderParameter):
-
-    _options = None
-
-    def __init__(self, value):
-        super(OptionsParameter, self).__init__('options')
-        self._options = value
-
-    def value(self):
-        return str(self._options)
-
-
-class OrderParameters(object):
-
-    parameters = list()
-
-    def __init__(self, order_string):
-        parms = json.loads(order_string)
-
-        self.parameters = [self._parameter_instance(name, value)
-                           for (name, value) in parms.items()]
+        # TODO TODO TODO
+        # This probably is not a failure condition since most options can be
+        # defaulted
+        if option not in self.valid_options.keys():
+            message = "[%s] is missing from order options" % option
+            raise OptionViolation(message)
 
 
-    def _parameter_instance(self, name, value):
+class LandsatOrderParameters(OrderParameters):
 
-        if name == 'orderid':
-            return OrderIdParameter(value)
-        elif name == 'scene':
-            return SceneParameter(value)
-        elif name == 'xmlrpc':
-            return XmlrcpParameter(value)
-        elif name == 'options':
-            return OptionsParameter('tulip.com')
-        else:
-            message = "[%s] is not a valid parameter" % name
-            raise NotImplementedError(message)
+    def __init__(self, *args, **kwarg):
+
+        self.valid_options = {
+            'keep_log': False
+        }
+
+        super(LandsatOrderParameters, self).__init__(*args, **kwarg)
 
 
-    def __iter__(self):
-        for item in self.parameters:
-            name = item.name()
-            if name != 'options':
-                yield (name, item.value())
+class ModisOrderParameters(OrderParameters):
+
+    def __init__(self, *args, **kwarg):
+
+        self.valid_options = {
+            'keep_log': False
+        }
+
+        super(ModisOrderParameters, self).__init__(*args, **kwarg)
+
+
+def instance(parms):
+    if 'scene' not in parms.keys():
+        message = "[scene] is missing from order parameters"
+        raise ParameterViolation(message)
+
+    _id = parms['scene'].lower().strip()
+
+    instances = {
+        'tm': (r'^lt[4|5]\d{3}\d{3}\d{4}\d{3}\w{3}\d{2}$',
+               LandsatOrderParameters),
+        'etm': (r'^le7\d{3}\d{3}\d{4}\d{3}\w{3}\d{2}$',
+                LandsatOrderParameters),
+
+        'mod09a1': (r'^mod09a1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod09ga': (r'^mod09ga\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod09gq': (r'^mod09gq\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod09q1': (r'^mod09q1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+
+        'mod13a1': (r'^mod13a1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod13a2': (r'^mod13a2\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod13a3': (r'^mod13a3\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'mod13q1': (r'^mod13q1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+
+        'myd09a1': (r'^myd09a1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd09ga': (r'^myd09ga\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd09gq': (r'^myd09gq\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd09q1': (r'^myd09q1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+
+        'myd13a1': (r'^myd13a1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd13a2': (r'^myd13a2\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd13a3': (r'^myd13a3\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters),
+        'myd13q1': (r'^myd13q1\.a\d{7}\.h\d{2}v\d{2}\.005\.\d{13}$',
+                    ModisOrderParameters)
+    }
+
+    for key in instances.iterkeys():
+        if re.match(instances[key][0], _id):
+            return instances[key][1](parms)
+
+    msg = "[%s] is not a supported sensor product" % parms['scene']
+    raise NotImplementedError(msg)
 
 
 if __name__ == '__main__':
-    order = OrderParameters('{ "orderid": "water", "scene": "town", "xmlrpc": "skip_xmlrpc", "options": { "keep_log": true } }')
+    '''
+    "scene": "LT42181092013069PFS00",
+    "scene": "LT52181092013069PFS00",
+    "scene": "LE72181092013069PFS00",
+    "scene": "MOD09A1.A2002041.h09v04.005.2007125045728",
+    '''
 
-    print dict(order)
-    print json.dumps(dict(order), indent=4, sort_keys=True)
+    order_string = \
+"""
+{
+    "options": {
+        "keep_log": true
+    },
+    "orderid": "water",
+    "scene": "LE72181092013069PFS00",
+    "xmlrpc": "skip_xmlrpc"
+}
+"""
 
-
-
-#    parameter_1 = order_parameter_instance('orderid', 'water')
-#    parameter_2 = order_parameter_instance('scene', 'town')
-#
-#    print parameter_1.orderid
-#
-#    igor = parameter_1.__dict__
-#    print igor
+    parms = instance(json.loads(order_string))
+    print json.dumps(parms, indent=4, sort_keys=True)
 
     sys.exit()
