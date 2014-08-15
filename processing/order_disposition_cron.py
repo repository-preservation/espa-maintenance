8dd9e4aad28ba15265893e30e11cf0905f022a42
Ronald Dilley <rdilley@usgs.gov>
2014-08-15 20:37:25 +0000

    Updated to make operational through the cron
    git-svn-id: http://espa.googlecode.com/svn/trunk@1040 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/order_disposition_cron.py
+++ processing/order_disposition_cron.py
@@ -1,14 +1,44 @@
+#! /usr/bin/env python
+
+'''
+    FILE: order_disposition_cron.py
+
+    PURPOSE: Processes order finalization and email generation for accepted
+             orders.
+
+    PROJECT: Land Satellites Data Systems Science Research and Development
+             (LSRD) at the USGS EROS
+
+    LICENSE: NASA Open Source Agreement 1.3
+
+    HISTORY:
+
+    Date              Programmer               Reason
+    ----------------  ------------------------ --------------------------------
+    ??/??/????        David V. Hill            Initial implementation.
+    Aug/2014          Ron Dilley               Made operational for cron
+'''
+
 import os
 import sys
 import xmlrpclib
+import traceback
+
 # espa-common objects and methods
 from espa_constants import EXIT_FAILURE
 from espa_constants import EXIT_SUCCESS
-import traceback
 
 from espa_logging import log
 
+
+# ============================================================================
 def determine_order_disposition():
+    '''
+    Description:
+      Interact with the web service to accomplish order finalization tasks
+      along with sending the initial emails out to the users after their
+      order has been accepted.
+    '''
 
     rpcurl = os.environ.get('ESPA_XMLRPC')
     server = None
@@ -25,24 +55,50 @@ def determine_order_disposition():
     if server is None:
         msg = "xmlrpc server was None... exiting"
         raise Exception(msg)
-        
+
     try:
         result = server.send_initial_emails()
         if not result:
             msg = "server.send_initial_emails() was not successful"
             raise Exception(msg)
-        
+
         result = server.finalize_orders()
         if not result:
             msg = "server.finalize_orders() result was not successful"
             raise Exception(msg)
-            
+
+    except xmlrpclib.ProtocolError, e:
+        log("A protocol error occurred: %s" % str(e))
+        tb = traceback.format_exc()
+        log(tb)
+
     except Exception, e:
         log("An error occurred finalizing orders: %s" % str(e))
         tb = traceback.format_exc()
         log(tb)
-        sys.exit(EXIT_FAILURE)
 
-    sys.exit(EXIT_SUCCESS)        
-        
-    
+    finally:
+        server = None
+# END - determine_order_disposition
+
+
+# ============================================================================
+if __name__ == '__main__':
+    '''
+    Description:
+      Execute the order disposition determination routine.
+    '''
+
+    # Check required variables that this script should fail on if they are not
+    # defined
+    required_vars = ['ESPA_XMLRPC']
+    for env_var in required_vars:
+        if (env_var not in os.environ or os.environ.get(env_var) is None
+                or len(os.environ.get(env_var)) < 1):
+
+            log("$%s is not defined... exiting" % env_var)
+            sys.exit(EXIT_FAILURE)
+
+    determine_order_disposition()
+
+    sys.exit(EXIT_SUCCESS)
