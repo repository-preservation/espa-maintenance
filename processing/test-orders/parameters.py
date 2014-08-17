d874e7e9d56f1a49c4019d97db5b907ea309c55d
Ronald Dilley <rdilley@usgs.gov>
2014-08-17 02:38:49 +0000

    Updated
    git-svn-id: http://espa.googlecode.com/svn/trunk@1045 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/test-orders/parameters.py
+++ processing/test-orders/parameters.py
@@ -4,6 +4,7 @@ import sys
 import re
 import logging
 import json
+from argparse import ArgumentParser
 
 
 # Exceptions - TODO TODO TODO This should be else-where
@@ -29,7 +30,6 @@ class Parameters(dict):
     valid_parameters = None
     valid_options = None
 
-
     def __init__(self, *args, **kwarg):
         '''
         Definition:
@@ -37,7 +37,6 @@ class Parameters(dict):
           underlying dict object.
         '''
         super(Parameters, self).__init__(*args, **kwarg)
-
 
     def _is_valid_parameter(self, parameter):
 
@@ -49,7 +48,6 @@ class Parameters(dict):
             message = "[%s] is not a valid parameter" % parameter
             raise ParameterViolation(message)
 
-
     def _is_valid_option(self, option):
 
         if not self.valid_options:
@@ -59,7 +57,6 @@ class Parameters(dict):
         if option not in self.valid_options.keys():
             message = "[%s] is not a valid option" % option
             raise OptionViolation(message)
-
 
     def _find_required_parameters(self, parameters):
 
@@ -71,7 +68,6 @@ class Parameters(dict):
             if parameter not in parameters:
                 message = "[%s] is missing from order parameters" % parameter
                 raise ParameterViolation(message)
-
 
     def _find_required_options(self, options):
 
@@ -101,7 +97,6 @@ class Options(dict):
 
     options = None
 
-
     def __init__(self, *args, **kwarg):
         '''
         Definition:
@@ -127,32 +122,293 @@ class Options(dict):
     # TODO TODO TODO - IMPLEMENT ME
 
 
-class Projection(dict):
+class Projection(object):
 
-    def __init__(self, name, value):
-        super(GeographicProjection, self).__init__(*args, **kwarg)
+    _target_projection = None
+    _options = None
+    _defaults = None
+    _required_options = None
 
-    def is_present(options):
+    def __init__(self, *args, **kwarg):
+        if self._required_options:
+            options = dict(*args, **kwarg)
+            self._options = dict()
+            for option in self._required_options:
+                if option not in options.keys():
+                    message = ("[%s] is required for target_projection"
+                               % option)
+                    raise OptionViolation(message)
+                else:
+                    option = str(option)
+                    value = options[option]
+                    if type(value) is unicode:
+                        if value.isnumeric():
+                            try:
+                                value = int(value)
+                            except ValueError:
+                                value = float(value)
+                        else:
+                            value = str(value)
+                    else:
+                        # It is already what I want
+                        pass
+                    self._options.update({option: value})
+        else:
+            self._options = None
+
+    def proj4(self):
         message = "You must implement this in the sub-class"
         raise NotImplementedError(message)
+
+    def defaults(self):
+        d = dict()
+        d.update({'target_projection': self._target_projection})
+        if self._defaults is not None:
+            d.update(self._defaults)
+
+        return d
+
+    def to_dict(self):
+        d = dict()
+
+        if self._target_projection:
+            d.update({'target_projection': self._target_projection})
+            if self._options is not None:
+                d.update(self._options)
+
+        return d
+# END - Projection
 
 
 class GeographicProjection(Projection):
 
-    default_options = {
-        'target_projection': 'latlon'
-    }
+    def __init__(self, *args, **kwarg):
+        self._target_projection = 'lonlat'
+        self._defaults = None
+        self._required_options = None
 
-    def __init__(self):
         super(GeographicProjection, self).__init__(*args, **kwarg)
 
-    def proj4(options):
-        pass
-#        if 'target_projection' in options.keys():
-#            if self.target_projection == 
+    def proj4(self):
+        '''
+        Description:
+          Builds a proj.4 string for geographic
+          gdalsrsinfo 'EPSG:4326'
+
+        Example:
+          +proj=longlat +datum=WGS84 +no_defs
+        '''
+
+        return "'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'"
+# END - GeographicProjection
 
 
-    # TODO TODO TODO - IMPLEMENT ME
+class UTMProjection(Projection):
+
+    def __init__(self, *args, **kwarg):
+        self._target_projection = 'utm'
+        self._defaults = {
+            'utm_north_south': None,
+            'utm_zone': None
+        }
+        self._required_options = ['utm_north_south', 'utm_zone']
+
+        super(UTMProjection, self).__init__(*args, **kwarg)
+
+    def proj4(self):
+        '''
+        Description:
+          Builds a proj.4 string for utm
+
+        Examples:
+          +proj=utm +zone=60 +ellps=WGS84 +datum=WGS84 +units=m +no_defs
+
+          +proj=utm +zone=39 +south +ellps=WGS72
+          +towgs84=0,0,1.9,0,0,0.814,-0.38 +units=m +no_defs
+        '''
+
+        # TODO - Found this example on the web for south (39), that
+        # TODO - specifies the datum instead of "towgs"
+        # TODO - gdalsrsinfo EPSG:32739
+        # TODO - +proj=utm +zone=39 +south +datum=WGS84 +units=m +no_defs
+        # TODO - It also seems that northern doesn't need the ellipsoid either
+        # TODO - gdalsrsinfo EPSG:32660
+        # TODO - +proj=utm +zone=60 +datum=WGS84 +units=m +no_defs
+
+        proj4 = ("'+proj=utm +zone=%i +ellps=WGS84 +datum=WGS84"
+                 " +units=m +no_defs'" % self._options['utm_zone'])
+        if self._options['utm_north_south'].lower() == 'south':
+            proj4 = ("'+proj=utm +zone=%i +south +ellps=WGS72"
+                     " +towgs84=0,0,1.9,0,0,0.814,-0.38 +units=m +no_defs'"
+                     % self._options['utm_zone'])
+
+        return proj4
+# END - UTMProjection
+
+
+def get_projection_instance(options):
+    '''
+    Description:
+        TODO TODO TODO
+    '''
+
+    if 'target_projection' in options.keys():
+        target_projection = options['target_projection']
+        if target_projection == 'lonlat':
+            return GeographicProjection(options)
+        elif target_projection == 'utm':
+            return UTMProjection(options)
+        # TODO TODO TODO - Write more projections
+        else:
+            message = ("[%s] projection not implemented"
+                       % target_projection)
+            raise NotImplementedError(message)
+    else:
+        return None
+# END - get_projection_instance
+
+
+class ImageExtents(object):
+
+    _options = None
+    _defaults = None
+    _required_options = ['maxx', 'maxy', 'minx', 'miny']
+
+    def __init__(self, *args, **kwarg):
+        options = dict(*args, **kwarg)
+
+        self._options = dict()
+        for option in self._required_options:
+            if option not in options.keys():
+                message = ("[%s] is required for image_extents" % option)
+                raise OptionViolation(message)
+            else:
+                option = str(option)
+                try:
+                    value = float(options[option])
+                except ValueError:
+                    message = ("[%s] is required to be numeric" % option)
+                    raise OptionViolation(message)
+
+                self._options.update({option: value})
+
+    def gdal_warp(self):
+        message = "You must implement this here  TODO TODO TODO"
+        raise NotImplementedError(message)
+
+    def defaults(self):
+        d = dict()
+
+        d.update({'image_extents': False})
+        for option in self._required_options:
+            d.update({option: None})
+
+        return d
+
+    def to_dict(self):
+        d = dict()
+
+        d.update({'image_extents': True})
+        d.update(self._options)
+
+        return d
+# END - ImageExtents
+
+
+def get_image_extents_instance(options):
+    '''
+    Description:
+        TODO TODO TODO
+    '''
+
+    if 'image_extents' in options.keys():
+        if options['image_extents']:
+            return ImageExtents(options)
+        else:
+            return None
+    else:
+        return None
+# END - get_image_extents_instance
+
+
+class WarpParameters(object):
+
+    _reproject = None
+    _defaults = {'reproject': False}
+    _required_options = ['reproject']
+
+    _projection = None
+    _image_extents = None
+    _resize = None
+
+    def __init__(self, *args, **kwarg):
+
+        options = dict(*args, **kwarg)
+
+        self._reproject = options['reproject']
+
+        if self._reproject:
+            # Get a tprojection if one was requested
+            self._projection = get_projection_instance(options)
+
+            # Get image_extents if one was requested
+            self._image_extents = get_image_extents_instance(options)
+
+            # Get resize options
+            #     resize_parameters(object)
+
+    def gdal_warp(self):
+        message = "You must implement this here  TODO TODO TODO"
+        raise NotImplementedError(message)
+
+    def defaults(self):
+        d = dict()
+
+        d.update(self._defaults)
+
+        if self._projection is not None:
+            d.update(self._projection.defaults())
+
+        if self._image_extents is not None:
+            d.update(self._image_extents.defaults())
+
+# TODO TODO TODO - Implement these
+#        if self._resize:
+#            d.update(self._resize.defaults()
+
+        return d
+
+    def to_dict(self):
+        d = dict()
+
+        if self._reproject:
+            d.update({'reproject': self._reproject})
+
+            if self._projection is not None:
+                d.update(self._projection.to_dict())
+
+            if self._image_extents is not None:
+                d.update(self._image_extent.to_dict())
+
+# TODO TODO TODO - Implement these
+#            if self._resize:
+#                d.update(self._resize.to_dict()
+
+        return d
+# END - WarpParameters
+
+
+def get_warp_instance(options):
+    '''
+    Description:
+        TODO TODO TODO
+    '''
+
+    if 'reproject' in options.keys():
+        return WarpParameters(options)
+    else:
+        return None
+# END - get_warp_instance
 
 
 class OrderParameters(Parameters):
@@ -382,32 +638,54 @@ def instance(parms):
 
 
 if __name__ == '__main__':
-    '''
-    "scene": "LT42181092013069PFS00",
-    "scene": "LT52181092013069PFS00",
-    "scene": "LE72181092013069PFS00",
-    "scene": "MOD09A1.A2002041.h09v04.005.2007125045728",
-    '''
 
     logger = logging.basicConfig(format=('%(asctime)s.%(msecs)03d %(process)d'
                                          ' %(levelname)-8s'
-                                         ' %(filename)s:%(lineno)d:%(funcName)s'
-                                         ' -- %(message)s'),
+                                         ' %(filename)s:%(lineno)d:'
+                                         '%(funcName)s -- %(message)s'),
                                  datefmt='%Y-%m-%d %H:%M:%S')
 
-    order_string = \
-"""
-{
-    "options": {
-        "keep_log": true
-    },
-    "orderid": "water",
-    "scene": "LE72181092013069PFS00",
-    "xmlrpcurl": "skip_xmlrpc"
-}
-"""
+    # Create a command line argument parser
+    description = "Configures and executes a test order"
+    parser = ArgumentParser(description=description)
+
+    # Add parameters
+    parser.add_argument('--order-file',
+                        action='store', dest='order_file', required=True,
+                        help="order file to process")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Avoid the creation of the *.pyc files
+    os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+    if not os.path.isfile(args.order_file):
+        logger.critical("Order file (%s) does not exist" % args.order_file)
+        sys.exit(1)
+
+    order_fd = open(args.order_file, 'r')
+
+    order_string = order_fd.read()
+    if not order_string:
+        logger.critical("Empty file exiting")
+        sys.exit(1)
 
     parms = instance(json.loads(order_string))
-    print json.dumps(parms, indent=4, sort_keys=True)
+    if parms:
+        print json.dumps(parms, indent=4, sort_keys=True)
 
-    sys.exit()
+    proj = get_projection_instance(parms['options'])
+    if proj:
+        print proj.proj4()
+        print proj.defaults()
+        print proj.to_dict()
+        print json.dumps(proj.to_dict(), indent=4, sort_keys=True)
+
+    warp_parms = get_warp_instance(parms['options'])
+    if warp_parms:
+        print warp_parms.defaults()
+        print warp_parms.to_dict()
+        print json.dumps(warp_parms.to_dict(), indent=4, sort_keys=True)
+
+    sys.exit(0)
