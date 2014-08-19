801a30a696e73e18b99360a5cb80c3bfa1125533
Ronald Dilley <rdilley@usgs.gov>
2014-08-19 17:58:17 +0000

    Updated
    git-svn-id: http://espa.googlecode.com/svn/trunk@1058 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/test-orders/run-test-order.py
+++ processing/test-orders/run-test-order.py
@@ -38,69 +38,91 @@ def build_argument_parser():
                         action='store_true', dest='keep_log', default=False,
                         help="keep the log file")
 
-    parser.add_argument('--order-file',
-                        action='store', dest='order_file', required=True,
-                        help="order file to process")
+    parser.add_argument('--request-file',
+                        action='store', dest='request_file', required=True,
+                        help="file containing request specifics")
+
+    parser.add_argument('--products-file',
+                        action='store', dest='products_file', required=True,
+                        help="file containing products to process")
 
     return parser
 # END - build_argument_parser
 
 
 # ============================================================================
-def process_test_order(order_file, env_vars, keep_log):
+def process_test_order(request_file, products_file, env_vars, keep_log):
     '''
     Description:
       Process the test order file.
     '''
 
-    tmp_order = 'tmp-' + order_file
-    order_fd = open(order_file, 'r')
-    tmp_fd = open(tmp_order, 'w')
-    order_id = order_file.split('.json')[0]
+    logger = logging.getLogger(__name__)
+
+    tmp_order = 'tmp-' + request_file
+
+    order_id = request_file.split('.json')[0]
 
     have_error = False
     error_msg = ''
-    while 1:
-        line = order_fd.readline()
-        if not line:
-            break
 
-        tmp_line = line
-
-        # Validate using our parameter object
-        order = parameters.instance(json.loads(line))
-        print json.dumps(order, indent=4, sort_keys=True)
-        scene = order['scene']
-
-        # Update the order for the developer
-        tmp = scene[:3]
-        is_modis = False
-        if tmp == 'MOD' or tmp == 'MYD':
-            is_modis = True
-
-        if not is_modis:
-            scene_path = env_vars['dev_data_dir']['value'] + '/' \
-                + order['scene']
-            scene_path += '.tar.gz'
-
-            if not os.path.isfile(scene_path):
-                error_msg = "Missing scene data (%s)" % scene_path
-                have_error = True
+    with open(products_file, 'r') as scenes_fd:
+        while (1):
+            product_name = scenes_fd.readline().strip()
+            if not product_name:
                 break
 
-        tmp_line = tmp_line.replace("ORDER_ID", order_id)
-        tmp_line = tmp_line.replace("DEV_DATA_DIRECTORY",
-                                    env_vars['dev_data_dir']['value'])
-        tmp_line = tmp_line.replace("DEV_CACHE_DIRECTORY",
-                                    env_vars['dev_cache_dir']['value'])
+            with open(request_file, 'r') as order_fd:
+                order_contents = order_fd.read()
+                if not order_contents:
+                    raise Exception("Order file [%s] is empty" % request_file)
 
-        tmp_fd.write(tmp_line)
+                # Validate using our parameter object
+                #order = parameters.instance(json.loads(order_contents))
+                #print json.dumps(order, indent=4, sort_keys=True)
 
-        # Validate again, since we modified it
-        parms = parameters.instance(json.loads(tmp_line))
+                with open(tmp_order, 'w') as tmp_fd:
 
-    order_fd.close()
-    tmp_fd.close()
+                    tmp_line = order_contents
+
+                    # Update the order for the developer
+                    tmp = product_name[:3]
+                    is_modis = False
+                    if tmp == 'MOD' or tmp == 'MYD':
+                        is_modis = True
+
+                    if not is_modis:
+                        product_path = ('%s/%s%s'
+                                        % (env_vars['dev_data_dir']['value'],
+                                           product_name, '.tar.gz'))
+
+                        if not os.path.isfile(product_path):
+                            error_msg = ("Missing product data (%s)"
+                                         % product_path)
+                            have_error = True
+                            break
+
+                    tmp_line = tmp_line.replace('\n', '')
+
+                    tmp_line = tmp_line.replace("ORDER_ID", order_id)
+                    tmp_line = tmp_line.replace("SCENE_ID", product_name)
+                    tmp_line = \
+                        tmp_line.replace("DEV_DATA_DIRECTORY",
+                                         env_vars['dev_data_dir']['value'])
+                    tmp_line = \
+                        tmp_line.replace("DEV_CACHE_DIRECTORY",
+                                         env_vars['dev_cache_dir']['value'])
+
+                    tmp_fd.write(tmp_line)
+
+                    # Validate again, since we modified it
+                    parms = parameters.instance(json.loads(tmp_line))
+                    print json.dumps(parms, indent=4, sort_keys=True)
+
+                # END - with tmp_order
+            # END - with request_file
+        # END - while (1)
+    # END - with products_file
 
     if have_error:
         print error_msg
@@ -159,11 +181,14 @@ if __name__ == '__main__':
         Main code for executing a test order.
     '''
 
-    logger = logging.basicConfig(format=('%(asctime)s.%(msecs)03d %(process)d'
-                                         ' %(levelname)-8s'
-                                         ' %(filename)s:%(lineno)d:%(funcName)s'
-                                         ' -- %(message)s'),
-                                 datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(format=('%(asctime)s.%(msecs)03d %(process)d'
+                                ' %(levelname)-8s'
+                                ' %(filename)s:%(lineno)d:%(funcName)s'
+                                ' -- %(message)s'),
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
 
     # Build the command line argument parser
     parser = build_argument_parser()
@@ -181,26 +206,34 @@ if __name__ == '__main__':
         env_vars[var]['value'] = os.environ.get(env_vars[var]['name'])
 
         if env_vars[var]['value'] is None:
-            print "Missing environment variable " + env_vars[var]['name']
+            logger.warning("Missing environment variable [%s]"
+                           % env_vars[var]['name'])
             missing_environment_variable = True
 
     # Terminate FAILURE if missing environment variables
     if missing_environment_variable:
-        print "Please fix missing environment variables"
+        logger.critical("Please fix missing environment variables")
         sys.exit(1)
 
     # Parse the command line arguments
     args = parser.parse_args()
 
-    if not os.path.isfile(args.order_file):
-        print "Order file (%s) does not exist" % args.order_file
+    if not os.path.isfile(args.request_file):
+        logger.critical("Request file [%s] does not exist" % args.request_file)
+        sys.exit(1)
+
+    if not os.path.isfile(args.products_file):
+        logger.critical("Products file [%s] does not exist"
+                        % args.products_file)
         sys.exit(1)
 
     # Avoid the creation of the *.pyc files
     os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 
-    if not process_test_order(args.order_file, env_vars, args.keep_log):
-        print "Order file (%s) failed to process" % args.order_file
+    if not process_test_order(args.request_file, args.products_file, env_vars,
+                              args.keep_log):
+        logger.critical("Request file (%s) failed to process"
+                        % args.request_file)
         sys.exit(1)
 
     # Terminate SUCCESS
