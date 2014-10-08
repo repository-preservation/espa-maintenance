f29224b4293f5627b9797f4f399c770b0947aaf7
Ronald Dilley <rdilley@usgs.gov>
2014-10-08 19:17:37 +0000

    Renamed ledaps error code to surface_reflectance
    git-svn-id: http://espa.googlecode.com/svn/trunk@1261 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/cdr_ecv_mapper.py
+++ processing/cdr_ecv_mapper.py
@@ -235,7 +235,7 @@ def process(args):
                                                  processing_location)
 
                     elif (e.error_code == ee.ErrorCodes.metadata
-                          or e.error_code == ee.ErrorCodes.ledaps
+                          or e.error_code == ee.ErrorCodes.surface_reflectance
                           or e.error_code == ee.ErrorCodes.browse
                           or e.error_code == ee.ErrorCodes.spectral_indices
                           or e.error_code == ee.ErrorCodes.create_dem
