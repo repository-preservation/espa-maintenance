da0afa0727c59ad2783625ac5caf2ad0d00cc736
Ronald Dilley <rdilley@usgs.gov>
2014-11-12 21:36:11 +0000

    Not needed anymore, they have been replaced by another mechanism
    git-svn-id: http://espa.googlecode.com/svn/trunk@1316 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- processing/tests/sir.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env bash
-#Author: David V Hill
-#Date: October 25, 2013
-#Purpose: Tests cdr_ecv.py for spectral indice reprojection and subsetting.
-
-/home/dhill/checkout/espa/trunk/espa/cdr_ecv.py --scene LT50450262011188PAC01 --order steinwand@usgs.gov-10212013-95639  --source_host localhost --destination_host localhost --destination_directory /data/tmp/sir --source_type level1  --projection '+proj=aea +lat_1=29.500000 +lat_2=45.500000 +lat_0=23.000000 +lon_0=-96.000000 +x_0=0.000000 +y_0=0.000000 +ellps=GRS80 +datum=nad83 +units=m +no_defs '  --set_image_extent -2152425.0,2877435.0,-1762425.0,3177435.0  --pixel_size 30.0 --pixel_unit meters --resample_method near --sr_ndvi --sr_nbr --sr_evi
--- processing/tests/t_ll.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/env bash
-
-/home/dhill/checkout/espa/trunk/espa/cdr_ecv.py --scene LT50450262011188PAC01 --order steinwand@usgs.gov-10212013-95639  --source_host localhost --destination_host localhost --destination_directory /data/tmp/ll --source_type level1  --projection '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' --pixel_size .0002695 --pixel_unit dd --resample_method near --band6 --toa 
