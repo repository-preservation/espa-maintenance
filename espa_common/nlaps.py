896240a3e8929f56fb5fea127754b6c0f3cc74a1
David Hill <dhill@usgs.gov>
2014-11-26 17:50:30 +0000

    pep8 edits
    git-svn-id: http://espa.googlecode.com/svn/trunk@1371 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- espa_common/nlaps.py
+++ espa_common/nlaps.py
@@ -2,12 +2,13 @@ import os
 
 ''' This is a simple lookup to determine if a Landsat 5 scene is TMA or not '''
 
+
 class NLAPS(object):
-    
+
     def __init__(self):
         self.path = os.path.dirname(__file__)
         self.path = os.path.join(self.path, 'nlaps.txt')
-        
+
         with open(self.path, 'rb') as nl:
             data = nl.read()
             self.names = data.split('\n')
@@ -17,7 +18,7 @@ def products_are_nlaps(product_list):
 
     if not isinstance(product_list, list):
         raise TypeError("product_list must be an instance of list()")
-        
+
     results = []
     for p in product_list:
         if p in NLAPS().names:
