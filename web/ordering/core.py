4ebdfe32281281c19bc1dec772c0ea2b9f6d0891
David Hill <dhill@usgs.gov>
2014-12-03 18:26:47 +0000

    fixed bug with select_related mapping value in core
    git-svn-id: http://espa.googlecode.com/svn/trunk@1395 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- web/ordering/core.py
+++ web/ordering/core.py
@@ -507,9 +507,9 @@ def get_products_to_process(record_limit=500,
 
     #optimize the query so it creates a join call rather than executing
     #multiple database calls for the related fields
-    select_related = ('order__orderid',
-                      'order__priority',
-                      'order__product_options',
+    select_related = ('order__orderid,'
+                      'order__priority,'
+                      'order__product_options,'
                       'userprofile__contactid')
 
     # use orderby for the orderby clause
