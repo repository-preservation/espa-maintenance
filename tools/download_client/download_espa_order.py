e2336f7163c18a509ebe1223103e50796daf7ffe
David Hill <dhill@usgs.gov>
2014-12-30 19:35:27 +0000

    Updated the download_client with the one that should have been included from the pre-espa 2.4 branch
    git-svn-id: http://espa.googlecode.com/svn/trunk@1558 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- tools/download_client/download_espa_order.py
+++ tools/download_client/download_espa_order.py
@@ -1,113 +1,138 @@
 #!/usr/bin/env python
 
-'''
+"""
 Author: David Hill
 Date: 01/31/2014
-Purpose: A simple python client that will download all available (completed)
-         scenes for a user order(s).
+Purpose: A simple python client that will download all available (completed) scenes for
+         a user order(s).
 
-Requires: Python feedparser and standard Python installation.
+Requires: Python feedparser and standard Python installation.     
 
-Version: 1.0 -- David Hill
-    Original Implementation
-Version: 1.1 -- Ron Dilley
-    Updated for pep8 compliance.
-    Updated to support resuming the downloads.
-'''
+Version: 1.0
+"""
 
-import os
-import sys
-import shutil
-import argparse
 import feedparser
 import urllib2
+import argparse
+import shutil
+import os
 
+class SceneFeed(object):
+    """SceneFeed parses the ESPA RSS Feed for the named email address and generates
+    the list of Scenes that are available"""
+    
+    def __init__(self, email, host="http://espa.cr.usgs.gov"):
+        """Construct a SceneFeed.
+        
+        Keyword arguments:
+        email -- Email address orders were placed with
+        host  -- http url of the RSS feed host
+        """
+        
+        self.email = email
+        
+        if not host.startswith('http://'):
+            host = ''.join(["http://", host])
+        self.host = host
+        
+        self.feed_url = "%s/ordering/status/%s/rss/" % (self.host, self.email)
+        
+        
+    def get_items(self, orderid='ALL'):
+        """get_items generates Scene objects for all scenes that are available to be
+        downloaded.  Supply an orderid to look for a particular order, otherwise all
+        orders for self.email will be returned"""
+        
+        #yield Scenes with download urls
+        feed = feedparser.parse(self.feed_url)
+                
+        for entry in feed.entries:
 
-def download_the_file(link, target):
+            #description field looks like this
+            #'scene_status:thestatus,orderid:theid,orderdate:thedate'
+            scene_order = entry.description.split(',')[1].split(':')[1]
 
-    print ("Copying %s to %s" % (link, target))
-    req = urllib2.urlopen(link)
-    with open(target, 'wb') as target_handle:
-        shutil.copyfileobj(req, target_handle)
+            #only return values if they are in the requested order            
+            if orderid == "ALL" or scene_order == orderid:
+                yield Scene(entry.link, scene_order)
+            
+                
+class Scene(object):
+    
+    def __init__(self, srcurl, orderid):
+    
+        self.srcurl = srcurl
+    
+        self.orderid = orderid
+        
+        parts = self.srcurl.split("/")
+     
+        self.filename = parts[len(parts) - 1]
+        
+        self.name = self.filename.split('.tar.gz')[0]
+        
+                  
+              
+class LocalStorage(object):
+    
+    def __init__(self, basedir):
+        self.basedir = basedir
+                    
+    def directory_path(self, scene):
+        return ''.join([self.basedir, os.sep, scene.orderid, os.sep])
+        
+    def scene_path(self, scene):
+        return ''.join([self.directory_path(scene), scene.filename])
+    
+    def tmp_scene_path(self, scene):
+        return ''.join([self.directory_path(scene), scene.filename, '.part'])
+    
+    def is_stored(self, scene):        
+        return os.path.exists(self.scene_path(scene))        
+    
+    def store(self, scene):
+        
+        if self.is_stored(scene): return
+                    
+        download_directory = self.directory_path(scene)
+        
+        #make sure we have a target to land the scenes
+        if not os.path.exists(download_directory):
+            os.makedirs(download_directory)
+            print ("Created target_directory:%s" % download_directory)
+        
+        req = urllib2.urlopen(scene.srcurl)
+
+        print ("Copying %s to %s" % (scene.name, download_directory))
+        
+        with open(self.tmp_scene_path(scene), 'wb') as target_handle:
+            shutil.copyfileobj(req, target_handle)
+        
+        os.rename(self.tmp_scene_path(scene), self.scene_path(scene))
+
 
 
 if __name__ == '__main__':
-
+    
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("-e", "--email", required=True,
-                        help=("email address for the user that"
-                              " submitted the order"))
-
-    parser.add_argument("-o", "--order", required=True,
-                        help=("which order to download"
-                              " (use ALL for every order)"))
-
-    parser.add_argument("-d", "--target_directory", required=True,
-                        help="where to store the downloaded scenes")
-
-    parser.add_argument("--resume", action='store_true', dest='resume',
-                        required=False, default=False,
-                        help="resume downloading of files")
-
+    
+    parser.add_argument("-e", "--email", 
+                        required=True,
+                        help="email address for the user that submitted the order)")
+                        
+    parser.add_argument("-o", "--order",
+                        required=True,
+                        help="which order to download (use ALL for every order)")
+                        
+    parser.add_argument("-d", "--target_directory",
+                        required=True,
+                        help="where to store the downloaded scenes")   
+    
     args = parser.parse_args()
-
-    # print args.email
-    # print args.order
-    # print args.target_directory
-    # print args.resume
-
-    # make sure we have a target to land the scenes
-    if not os.path.exists(args.target_directory):
-        os.makedirs(args.target_directory)
-        print ("Created target_directory:%s" % args.target_directory)
-
-    # build the url for the order and get it into a feedparser object
-    feed_url = "http://espa.cr.usgs.gov/ordering/status/%s/rss/" % args.email
-    feed = feedparser.parse(feed_url)
-
-    # look through all their links and see if its the right order
-    target_links = []
-    if args.order == "ALL":
-        target_links = [entry.link for entry in feed.entries]
-    else:
-        # user asked for a specific order so go look for it
-        target_links = [entry.link for entry in feed.entries
-                        if args.order in entry.description]
-
-    # stop if theres nothing to do
-    if len(target_links) == 0:
-        print("No scenes available to download for %s" % args.order)
-        exit()
-    else:
-        print("Found %i scenes to download in order:%s"
-              % (len(target_links), args.order))
-
-    # rip through the links and download them using shutil to control
-    # memory usage.
-    for link in target_links:
-        # name target file same as source file
-        parts = link.split('/')
-        filename = parts[len(parts) - 1]
-        target = os.path.join(args.target_directory, filename)
-
-        if not args.resume:
-            download_the_file(link, target)
-
-        elif not os.path.exists(target):
-            download_the_file(link, target)
-
-        else:
-            req = urllib2.urlopen(link)
-            stat_info = os.stat(target)
-
-            source_length = int(req.headers['content-length'])
-            target_length = int(stat_info.st_size)
-
-            if target_length == source_length:
-                print ("Skipping %s already downloaded" % target)
-            else:
-                download_the_file(link, target)
-
-    print("Scene downloads complete.")
-    sys.exit(0)
+    
+    storage = LocalStorage(args.target_directory)
+    
+    for scene in SceneFeed(args.email).get_items(args.order):
+        storage.store(scene)
+        
+    
