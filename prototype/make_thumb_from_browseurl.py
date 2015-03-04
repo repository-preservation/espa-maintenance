21cda7a2291a07c093f60c441c9f7e6b9916885c
David Hill <dhill@usgs.gov>
2015-03-04 18:56:37 +0000

    added a quick little demo that pulls landsat browse and builds a clipped out square image that we could use on our webpages
    git-svn-id: http://espa.googlecode.com/svn/trunk@1690 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

---
+++ prototype/make_thumb_from_browseurl.py
@@ -0,0 +1,33 @@
+import requests
+import shutil
+import os
+from PIL import Image
+
+url = 'http://earthexplorer.usgs.gov/browse/landsat_8/2015/157/071/LC81570712015056LGN00.jpg'
+target_file = '/tmp/pic.jpg'
+final_image = '/tmp/a.jpg'
+
+r = requests.get(url, stream=True)
+if r.status_code == 200:
+    with open(target_file, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+
+
+#now clip the image to what we need
+target_width = 700 
+target_height = 650
+
+try:
+    im = Image.open(target_file)
+    h, w = im.size
+    left = w/2 - (target_width/2)
+    right = w - left
+    top = h/2 - (target_height/2)
+    bottom = h - top
+
+    box = (left, top, right, bottom)
+    pi = im.crop(box)
+    pi.save(final_image)
+finally:
+    os.remove(target_file)
