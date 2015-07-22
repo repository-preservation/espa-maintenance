600473b2f2b843c42176a31b942b2e19e6e68e38
David Hill <dhill@usgs.gov>
2015-07-22 10:33:48 -0500

    added environment to the mapper files on job submission

--- scheduling/ondemand_cron.py
+++ scheduling/ondemand_cron.py
@@ -186,6 +186,7 @@ def process_requests(args, logger_name, queue_priority, request_priority):
                  '-file', '%s/espa-site/processing/solr.py' % home_dir,
                  '-file', '%s/espa-site/processing/staging.py' % home_dir,
                  '-file', '%s/espa-site/processing/statistics.py' % home_dir,
+                 '-file', '%s/espa-site/processing/environment.py' % home_dir,
                  '-file', '%s/espa-site/processing/transfer.py' % home_dir,
                  '-file', '%s/espa-site/processing/warp.py' % home_dir,
                  '-file', ('%s/espa-site/espa_common/logger_factory.py'
