c486b2eed4787ba154030ecd2fed0c237fcf54ea
klsmith-usgs <klsmith@usgs.gov>
2015-12-23 08:28:31 -0600

    Added standard email retrieval query

--- utils.py
+++ utils.py
@@ -1,23 +1,30 @@
 import smtplib
 from email.mime.text import MIMEText
+import ConfigParser
+import os
+import datetime
+import subprocess
+
+from dbconnect import DBConnect
 
 
-CFG_NFO = "/home/{0}/.cfgnfo"
-
-def get_cfg(user):
+def get_cfg():
     """
     Retrieve the configuration information from the .cfgnfo file
-    Will need to be made more robust if the file changes
+    located in the current user's home directory
 
     :return: dict
     """
-    cfg_info = {}
-    with open(CFG_NFO.format(user), 'r') as f:
-        for line in f:
-            spl = line.split('=')
+    cfg_path = os.path.join(os.path.expanduser('~'), '.cfgnfo')
 
-            if len(spl) > 1:
-                cfg_info[spl[0]] = line[len(spl[0]) + 1:]
+    cfg_info = {}
+    config = ConfigParser.ConfigParser()
+    config.read(cfg_path)
+
+    for sect in config.sections():
+        cfg_info[sect] = {}
+        for opt in config.options(sect):
+            cfg_info[sect][opt] = config.get(sect, opt)
 
     return cfg_info
 
@@ -34,7 +41,6 @@ def send_email(sender, recipient, subject, body):
     :type subject: string
     :param body: success or failure message to be passed
     :type body: string
-    :return:
     """
     # This does not need to be anything fancy as it is used internally,
     # as long as we can see if the script succeeded or where it failed
@@ -47,3 +53,29 @@ def send_email(sender, recipient, subject, body):
     smtp = smtplib.SMTP("localhost")
     smtp.sendmail(sender, recipient, msg.as_string())
     smtp.quit()
+
+
+def get_email_addr(dbinfo, who):
+    sql = "select value from configuration where key = 'email.%s'"
+
+    with DBConnect(**dbinfo) as db:
+        db.select(sql, who)
+        out = db[0][0]
+
+    return out
+
+
+def backup_cron():
+    """
+    Make a backup of the current user's crontab
+    to /home/~/backups/
+    """
+    bk_path = os.path.join(os.path.expanduser('~'), 'backups')
+    if not os.path.exists(bk_path):
+        os.makedirs(bk_path)
+
+    ts = datetime.datetime.now()
+    cron_file = ts.strftime('crontab-%m%d%y-%H%M%S')
+
+    with open(os.path.join(bk_path, cron_file), 'w') as f:
+        subprocess.call(['crontab', '-l'], stdout=f)
