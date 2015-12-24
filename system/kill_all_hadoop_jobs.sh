18e1889ac99d4d08005939d634290eac67c445d9
David Hill <dhill@usgs.gov>
2015-12-24 11:28:02 -0600

    removing files from original location, adding readme

--- system/kill_all_hadoop_jobs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-for x in `hadoop job -list|grep job_|awk '{print $1}'`;do hadoop job -kill $x;done
--- system/list_cluster_java_processes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-for x in `cat ~/bin/hadoop/conf/slaves`;do echo ""; echo "Java Processes for $x"; ssh -q $x jps; done
-
