828ed3fe7aeeb93a6f0383c4a89683be989dbfa0
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2019-09-09 15:02:59 -0500

    added comments

--- maintenance/run-metrics.sh
+++ maintenance/run-metrics.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e
-
 # Helper script to automate the different reports (All, Landsat, MODIS, VIIRS)
+
+set -e  # bash option -e: Abort script at first error,
+        # when a command exits with non-zero status
+        # (except in until or while loops, if-tests, list constructs)
+
 logdir=~/monthly-logs/
 
-mkdir -p $logdir
+mkdir -p $logdir  # -p: no error if existing, make parent directories as needed
 python lsrd_stats.py -e ops -c ~/.usgs/.cfgnfo_metrics -d $logdir
 python lsrd_stats.py -e ops -c ~/.usgs/.cfgnfo_metrics -d $logdir --sensors MODIS
 python lsrd_stats.py -e ops -c ~/.usgs/.cfgnfo_metrics -d $logdir --sensors VIIRS
